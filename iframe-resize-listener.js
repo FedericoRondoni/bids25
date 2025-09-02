9e084484eaf84620a2c7a0b1b7e3b7be6c945364
Federico <72614374+FedericoRondoni@users.noreply.github.com>
2025-09-02 16:44:45 +0200

    Delete iframe-resize-listener.js

--- iframe-resize-listener.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("message", function(event) {
-  if (event.data && event.data.brellaHeight) {
-    var iframe = document.getElementById("brella-frame");
-    if (iframe) {
-      iframe.style.height = event.data.brellaHeight + "px";
-    }
-  }
-});
